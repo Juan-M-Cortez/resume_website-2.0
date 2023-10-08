cbb1c7cb39317aef16cac11e79663181824c91cb
Juan Cortez <j.cortez.jmc@gmail.com>
2023-10-07 18:54:39 -0600

    personal video

--- src/container/Header/Banner/Banner.jsx
+++ src/container/Header/Banner/Banner.jsx
@@ -1,12 +1,43 @@
 import compilerImg from '../../../assets/compilerImg2.png';
 import TypingAnimation from './TypingAnimation/TypingAnimation';
-import React from 'react';
+import IntroVideo from '../../../assets/sample.mp4';
+import React, {useRef, useEffect} from 'react';
 import './Banner.scss';
 
 const Banner = () => {
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const handleLoadedMetadata = () => {
+      // When the video's metadata is loaded (after page load),
+      // unmute the video by setting the volume to a non-zero value
+      videoElement.volume = 1; // Set the volume to unmute (0 is muted)
+    };
+
+    videoElement.addEventListener('loadedmetadata', handleLoadedMetadata);
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      videoElement.removeEventListener('loadedmetadata', handleLoadedMetadata);
+    };
+  }, []);
+
   return (
     <div className='app__banner'>
-      <TypingAnimation />
+      {/*-- <TypingAnimation /> */}
+
+      {/* Banner Video */}
+      <div className='video-container'>
+        {/*<video controls autoPlay muted>*/}
+        <video autoPlay controls ref={videoRef}>
+          <source src={IntroVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
     </div>
   );
 };
